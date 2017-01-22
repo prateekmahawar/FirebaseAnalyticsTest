a7d49925bc8efb255a770ea1ff7dc6291c396da0
Prateek Mahawar <PrateekMahawar@gmail.com>
2017-01-22 23:01:33 +0530

    Report Uploaded

--- FirebaseAnalyticsTest/ViewController.swift
+++ FirebaseAnalyticsTest/ViewController.swift
@@ -7,19 +7,23 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseCrash
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+   
+    }
+    @IBAction func CrashBtnTapped(_ sender: Any) {
+        print("Dank THis is Second crash for today")
+        FIRCrashMessage("Dank this is the next line which should be printed in firebase")
+     print("Fatal Error Test ")
+        abort()
+
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
 }
 
---
+++ Pods/Target Support Files/Pods-FirebaseAnalyticsTest/Pods-FirebaseAnalyticsTest-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_FirebaseAnalyticsTestVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_FirebaseAnalyticsTestVersionString[];
+
